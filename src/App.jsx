de7ad33342f6845f051f970b642a93441be8dc78
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-06 02:49:20 +0330

    fix errorElement for router

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
