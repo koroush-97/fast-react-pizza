8264280af2ec345269a0e717152faea29d49bc93
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-06 01:08:39 +0330

    fix navigation hook to use loader for our loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,11 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
       <Header />
