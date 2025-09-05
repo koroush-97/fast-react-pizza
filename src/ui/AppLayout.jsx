655c75abd1998e41e192d939bde3bef9809226c5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-06 02:33:07 +0330

    fix error handleing and set error component for our router

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,7 +1,7 @@
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
 import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
 
 export default function AppLayout() {
   const navigation = useNavigation();
