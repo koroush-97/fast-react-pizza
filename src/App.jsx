9b10509276169d6eacf88e06e304a98aa7636c97
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-05 14:51:45 +0330

    add new route : HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
   return <div>Hello vite</div>;
 }
