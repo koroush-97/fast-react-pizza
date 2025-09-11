f9772ceb61e7b55369942904e2e8f026722b17e2
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-11 19:33:33 +0330

    fix error  css for chickbox

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -74,7 +74,7 @@ function CreateOrder() {
 
         <div>
           <input
-            className="h-6 w-6 accent-yellow-400"
+            className="h-6 w-6 border-yellow-400 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
