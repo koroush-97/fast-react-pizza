800b6f5b83bd644518b39fbaffd5f26561a61d19
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-11 11:24:09 +0330

    add username component to Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,8 +7,7 @@ export default function Header() {
       <Link to="/"  className="uppercase tracking-widest"> Fast React Pizza Co. </Link>
 
       <SearchOrder />
-
-      <p>koroush</p>
+      <SearchOrder />
     </header>
   );
 }
