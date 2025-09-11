4650ad4881b30358db99acf8d0d2e00f4dd3d0cf
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-11 13:09:44 +0330

    set all  flex box style for components : Home - CartOverview - Header

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200 px-4 py-4 uppercase sm:px-6" >
+    <div className="bg-stone-800 text-stone-200 px-4 py-4 uppercase sm:px-6 flex items-center justify-between" >
       <p className="font-semibold  text-stone-300 space-x-4 sm:space-x-6 md:text-base" > 
         <span>23 pizzas</span>
         <span>$23.45</span>
