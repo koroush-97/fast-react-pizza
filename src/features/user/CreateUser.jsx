6cd6fe9d558a4db6fc9753081fa864e156552a1a
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 14:36:46 +0330

    create Button component and give it custom css

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -9,19 +9,21 @@ function CreateUser() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className='mb-4 text-sm text-stone-600 md:text-base'>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        className='w-72 '
+        className="w-72"
       />
 
       {username !== '' && (
         <div>
-          <button>Start ordering</button>
+          <b>Start ordering</b>
         </div>
       )}
     </form>
