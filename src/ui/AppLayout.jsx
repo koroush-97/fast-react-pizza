25f80c9d25564088cf70bd50c13298052ae938e7
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 16:43:39 +0330

    add a layer out of Loader main

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,24 +1,23 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 export default function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
-      {isLoading && <Loader />}
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {true && <Loader />}
 
       <Header />
-    <div className="overflow-y-auto ">
-        <main className="mx-auto max-w-3xl ">
-        <Outlet />
-      </main>
+      <div className="overflow-y-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-    </div>
-      
       <CartOverview />
     </div>
   );
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,3 +1,9 @@
-export default function Loader() {
-  return <div className="loader"></div>;
+function Loader() {
+  return (
+    <div className="absolute">
+      <div className="loader"></div>;
+    </div>
+  );
 }
+
+export default Loader;
