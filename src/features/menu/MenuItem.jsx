3a3f76c139663bd9b5fec8cd1a3085d8b0b382ce
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 19:51:32 +0330

    fix font and text style for button component in each item

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,4 @@
+import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 // eslint-disable-next-line
 function MenuItem({ pizza }) {
@@ -11,13 +12,13 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
-      <div className="flex flex-col">
+      <div className="flex grow flex-col">
         <p className="font-medium">{name}</p>
-        <p className="text-sm italic text-stone-500">
+        <p className="text-sm capitalize italic text-stone-500">
           {/* eslint-disable-next-line */}
           {ingredients.join(', ')}
         </p>
-        <div className="mt-auto">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
@@ -25,6 +26,8 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+
+          <Button> Add to cart </Button>
         </div>
       </div>
     </li>
