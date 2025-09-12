ff9b9bb6fa5da1ffee9dcb4c5a07071116dcfdae
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 15:09:17 +0330

    use Link of react  router dom to stop rerenders

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,5 +1,7 @@
-function LinkButton() {
-  return <div>LinkButton</div>;
+import { Link } from 'react-router-dom';
+
+function LinkButton({ children, to }) {
+  return <Link to="/order/new">{children}</Link>;
 }
 
 export default LinkButton;
