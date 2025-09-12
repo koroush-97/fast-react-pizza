63afa756fe6a83fda8515e8b1c996c3b8dc33de4
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 15:10:46 +0330

    fix link into LinkButton

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
-  return <Link to="/order/new">{children}</Link>;
+  return (
+    <Link to={to} className="text-sm text-blue-500 hover:text-blue-800">
+      {children}
+    </Link>
+  );
 }
 
 export default LinkButton;
