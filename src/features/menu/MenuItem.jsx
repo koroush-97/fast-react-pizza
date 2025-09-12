56034617e97a24cb1e264ceac88c32826628f3ec
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 19:20:08 +0330

    fix ui MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -5,12 +5,12 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
+    <li className="flex gap-4 py-2">
       <img src={imageUrl} alt={name} className="h-24" />
-      <div>
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
-        <div>
+        <div className="mt-auto">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
       </div>
