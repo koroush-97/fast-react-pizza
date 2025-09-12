6b4dbca871d74f66f28ac21e8ab3358b2b040ade
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 15:44:00 +0330

    Edit Button Component and add to for routeing

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,12 +2,18 @@ import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 function Button({ children, disabled, to }) {
-  if (to) return <Link>{children}</Link>;
+  const classname =
+    'inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4';
+
+  if (to)
+    return (
+      <Link to={to} className={classname}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
-    >
+    <button disabled={disabled} className={classname}>
       {children}
     </button>
   );
