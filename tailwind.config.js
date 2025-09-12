5f0f993b20f26e7eb343e4502b8e1a5583097126
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 17:18:53 +0330

    replace sans font with our custom font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        robotoMono: ["'Roboto Mono'", 'monospace'],
+        sans: ["'Roboto Mono'", 'monospace'],
       },
     },
   },
