445680508bd7777210c1c86c5b7dedeff6c0c907
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-12 15:12:59 +0330

    fix proptypes of LinkButton props

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,5 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import PropTypes from 'prop-types';
+
+LinkButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired,
+};
+
 function LinkButton({ children, to }) {
   return (
     <Link to={to} className="text-sm text-blue-500 hover:text-blue-800">
