f34d0012905ed4c0d4700c4ef2f6868b6f3ab232
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-14 15:41:51 +0330

    set custome stayle for mainc reaterder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,8 +48,8 @@ function CreateOrder() {
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
           <input className="input" type="text" name="customer" required />
         </div>
 
