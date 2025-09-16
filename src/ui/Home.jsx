677574bce2b34ddf6335215eae0fa0614ae8102b
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-16 17:58:48 +0330

    match ui order person with person name

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,17 +1,27 @@
-import CreateUser from "../features/user/CreateUser";
+import { useSelector } from 'react-redux';
+import CreateUser from '../features/user/CreateUser';
+import Username from '../features/user/Username';
+import Button from './Button';
 
 function Home() {
+  const userName = useSelector((state) => state.user.username);
+
   return (
-    <div className="my-10 text-center sm:my-16 px-4"  >
-      <h1 className=" mb-8  text-xl font-semibold md:text-3xl">
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
-
-        <span className="text-yellow-500" >
-       Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {userName === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering , {userName}
+        </Button>
+      )}
     </div>
   );
 }
