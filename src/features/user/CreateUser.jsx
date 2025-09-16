b9ca5b913943a3d8573ee622f8727762298d1068
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-16 17:39:38 +0330

    add  dispatch hook  to actioncreator function

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch();
   }
 
   return (
