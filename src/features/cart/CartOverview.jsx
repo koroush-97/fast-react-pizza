0efdafd81bda9a665c39c62f52bfb982ee10cdc7
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-17 21:20:23 +0330

    implement logic of total pizza item in totalCartQuantity

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function CartOverview() {
+  const totalCartQuantity = useSelector((state) =>
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0),
+  );
+
   return (
-    <div className="bg-stone-800 text-stone-200 px-4 py-4 uppercase sm:px-6 flex items-center justify-between" >
-      <p className="font-semibold  text-stone-300 space-x-4 sm:space-x-6 md:text-base" > 
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 md:text-base">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,7 +1,10 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 // eslint-disable-next-line
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   // eslint-disable-next-line
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
@@ -13,6 +16,7 @@ function MenuItem({ pizza }) {
       unitPrice,
       totalPrice: unitPrice * 1,
     };
+    dispatch(addItem(newItem));
   }
 
   return (
