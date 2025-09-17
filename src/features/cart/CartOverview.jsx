cf47889ea1205a299ac116484d419f82ff966010
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-17 21:39:22 +0330

    fix bug of getTotalCartPrice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,9 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getTotalCartPrice } from './cartSlice';
+import { getTotalCartQuantity } from './cartSlice';
 
 function CartOverview() {
-  const totalCartQuantity = useSelector(getTotalCartPrice);
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
 
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6">
