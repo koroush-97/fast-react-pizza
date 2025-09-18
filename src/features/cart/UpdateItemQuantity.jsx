47460826350c4e94a1ba87c0a0103e8320625eb5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 15:04:28 +0330

    fix bug of currentQuantity

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -2,17 +2,18 @@ import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 // eslint-disable-next-line
-export default function UpdateItemQuantity({ pizzaId }) {
+export default function UpdateItemQuantity({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex items-center gap-1 md:gap-3">
+    <div className="flex items-center gap-2 md:gap-3">
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
       </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(increaseItemQuantity(pizzaId))}
