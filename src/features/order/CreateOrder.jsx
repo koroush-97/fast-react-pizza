b1c446adad39e5d741eff1ce78e32e71db56605c
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 17:47:48 +0330

    impelement condition for empty cart

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,35 +3,12 @@ import { Form, useActionData, useNavigation } from 'react-router-dom';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
 import { getCart } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
   const userName = useSelector((state) => state.user.username);
@@ -43,6 +20,9 @@ function CreateOrder() {
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
+  console.log('cart ===', cart);
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
