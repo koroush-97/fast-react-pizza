bc06dfb0c49bd5164d7fc5981ffaf84be887dbce
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 14:35:01 +0330

    write logic of increase and decrease in UpdateItemQuantity

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,15 +1,22 @@
 import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 // eslint-disable-next-line
 export default function UpdateItemQuantity({ pizzaId }) {
   const dispatch = useDispatch();
 
   return (
     <div className="flex items-center gap-1 md:gap-3">
-      <Button type="round" onClick={() => dispatch()}>
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
         -
       </Button>
-      <Button type="round" onClick={() => dispatch()}>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
         +
       </Button>
     </div>
