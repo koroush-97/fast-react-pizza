b2d4e7fc96b0cfe7f35149b41ad472aca0e5521d
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 13:41:07 +0330

    impelement  instracture basis logic for UpdateItemQuantity component

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,3 +1,12 @@
-export default function UpdateItemQuantity() {
-  return <div>UpdateItemQuantity</div>;
+import Button from '../../ui/Button';
+
+function UpdateItemQuantity({ pizzaId }) {
+  return (
+    <div>
+      <Button type="round">-</Button>
+      <Button type="round">+</Button>
+    </div>
+  );
 }
+
+export default UpdateItemQuantity;
