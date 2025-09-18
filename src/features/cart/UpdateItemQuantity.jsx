bd486162d775c0477a2ed821dcc25c38c2eedef3
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 14:32:49 +0330

    write new logic for Buttons of UpdateItemQuantity

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,10 +1,17 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 // eslint-disable-next-line
 export default function UpdateItemQuantity({ pizzaId }) {
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <Button type="round">-</Button>
-      <Button type="round">+</Button>
+    <div className="flex items-center gap-1 md:gap-3">
+      <Button type="round" onClick={() => dispatch()}>
+        -
+      </Button>
+      <Button type="round" onClick={() => dispatch()}>
+        +
+      </Button>
     </div>
   );
 }
