595bad22d5d53b46d7dab986196c93f5941da9a8
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 13:20:32 +0330

    add Optional chaining for  getCurrentQuantityById

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -55,6 +55,6 @@ export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCurrentQuantityById = (id) => (state) =>
-  state.cart.cart.find((item) => item.pizzaId === id);
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
 
 // 'resrlect'
