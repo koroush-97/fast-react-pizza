0f11ce098918b509c5486d6b939f6560a6631d2e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-18 13:54:04 +0330

    write new custom type round

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,6 +10,7 @@ function Button({ children, disabled, to, type, onClick }) {
     small: base + ' py-2 px-2 md:px-5 md:py-2.5 text-xs',
     secondary:
       'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 hover:text-stone-800 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed  px-4 py-3  md:px-6 md:py-4',
+    round: base + ' py-1 px-2.5 md:px-3.5 md:py-2 text-sm',
   };
 
   if (to)
