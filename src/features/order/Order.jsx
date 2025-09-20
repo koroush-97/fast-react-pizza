3861c3a1c3d9266f1103e64feef7ed99f85654a5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-20 12:26:49 +0330

    prevent duplicate menu fetch by checking fetcher idle state

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -14,9 +14,12 @@ function Order() {
 
   const fetcher = useFetcher();
 
-  useEffect(function () {
-    if (!fetcher.data) fetcher.load('/menu');
-  }, []);
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.idle === 'idle') fetcher.load('/menu');
+    },
+    [fetcher],
+  );
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
