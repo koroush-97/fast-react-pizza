24cd5e7fd5f12c1d655e56c7f3e5f95563c63e36
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-20 13:40:20 +0330

    capsolate UpdateOrder mani in fetcher.Form

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -8,6 +8,7 @@ import {
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
@@ -88,6 +89,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,3 +1,14 @@
-export default function UpdateOrder() {
-  return <div>UpdateOrder</div>;
+import { useFetcher } from 'react-router-dom';
+import Button from '../../ui/Button';
+
+function UpdateOrder({ order }) {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary"> Make priority </Button>;
+    </fetcher.Form>
+  );
 }
+
+export default UpdateOrder;
