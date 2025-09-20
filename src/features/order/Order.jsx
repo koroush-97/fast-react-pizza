523367b876e051d90ee3599fc8498aee7ed508a7
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-09-20 13:10:23 +0330

    fix bug of ingredients

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -68,7 +68,8 @@ function Order() {
             key={item.pizzaId}
             isLoadingIngredients={fetcher.state === 'loading'}
             ingredients={
-              fetcher.data?.find((el) => el.id === item.pizzaId).ingredients
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
             }
           />
         ))}
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,10 +1,11 @@
 import { formatCurrency } from '../../utils/helpers';
-
+// eslint-disable-next-line
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  // eslint-disable-next-line
   const { quantity, name, totalPrice } = item;
 
   return (
-    <li className="py-3">
+    <li className="space-y-1 py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name} order
